9d99a1c65a994c2f10b4e32c7d34018faa59c732
Joost Toben <joosttoben@hotmail.com>
2021-12-06 17:23:54 +0100

    Add solution day 4.

---
+++ Day4/BingoBoard.cs
@@ -0,0 +1,100 @@
+﻿namespace Day4
+{
+    internal class BingoBoard
+    {
+        private bool[,] _markedPositions = new bool[5, 5];
+
+        public int[,] Numbers { get;} = new int[5, 5];
+
+        public void MarkNumber(int number)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if (Numbers[x, y] == number)
+                    {
+                        _markedPositions[x, y] = true;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public bool IsFinished()
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                bool lineIsFinished = true;
+                for (int y = 0; y < 5; y++)
+                {
+                    if (!_markedPositions[x, y])
+                    {
+                        lineIsFinished = false;
+                        break;
+                    }
+                }
+
+                if (lineIsFinished)
+                {
+                    return true;
+                }
+            }
+
+            for (int y = 0; y < 5; y++)
+            {
+                bool rowIsFinished = true;
+                for (int x = 0; x < 5; x++)
+                {
+                    if (!_markedPositions[x, y])
+                    {
+                        rowIsFinished = false;
+                        break;
+                    }
+                }
+
+                if (rowIsFinished)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public int GetWinningValue()
+        {
+            int value = 0;
+            for (int x = 0; x < 5; x++)
+            {
+                for (int y = 0; y < 5; y++)
+                {
+                    if (!_markedPositions[x, y])
+                    {
+                        value += Numbers[x, y];
+                    }
+                }
+            }
+
+            return value;
+        }
+        
+        public static BingoBoard FromString(string boardAsString)
+        {
+            var board = new BingoBoard();
+
+            var lines = boardAsString.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            for (int x = 0; x < lines.Length; x++)
+            {
+                var numbers = lines[x].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                for (int y = 0; y < numbers.Length; y++)
+                {
+                    board.Numbers[x, y] = numbers[y];
+                }
+            }
+
+            return board;
+        }
+    }
+}
--- Day4/Program.cs
+++ Day4/Program.cs
@@ -4,18 +4,59 @@
     {
         static void Main(string[] args)
         {
-            var input = File.ReadAllLines("input.txt");
+            var input = File.ReadAllText("input.txt").Split(new string[] { Environment.NewLine + Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            var bingoNumbers = input[0].Split(',').Select(int.Parse).ToList();
 
-            PartOne();
-            PartTwo();
+            PartOne(bingoNumbers, input.Skip(1).Select(x => BingoBoard.FromString(x)).ToList());
+            PartTwo(bingoNumbers, input.Skip(1).Select(x => BingoBoard.FromString(x)).ToList());
         }
 
-        private static void PartOne()
+        private static void PartOne(List<int> bingoNumbers, List<BingoBoard> bingoBoards)
         {
+            BingoBoard? winningBoard = null;
+            foreach (var number in bingoNumbers)
+            {
+                foreach (var board in bingoBoards)
+                {
+                    board.MarkNumber(number);
+
+                    if (board.IsFinished())
+                    {
+                        winningBoard = board;
+                        break;
+                    }
+                }
+
+                if (winningBoard != null)
+                {
+                    Console.WriteLine(winningBoard.GetWinningValue() * number);
+                    break;
+                }
+            }
         }
 
-        private static void PartTwo()
+        private static void PartTwo(List<int> bingoNumbers, List<BingoBoard> bingoBoards)
         {
+            BingoBoard? winningBoard = null;
+            foreach (var number in bingoNumbers)
+            {
+                foreach (var board in bingoBoards)
+                {
+                    board.MarkNumber(number);
+                }
+
+                bingoBoards = bingoBoards.Where(x => !x.IsFinished()).ToList();
+
+                if (bingoBoards.Count == 1)
+                {
+                    winningBoard = bingoBoards[0];
+                } else if (bingoBoards.Count == 0)
+                {
+                    Console.WriteLine(winningBoard!.GetWinningValue() * number);
+                    break;
+                }
+            }
+
         }
     }
 }
