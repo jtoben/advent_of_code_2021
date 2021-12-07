072c7bb7da545b906e2878a3a42b7c2204397c76
Joost Toben <joosttoben@hotmail.com>
2021-12-07 16:11:22 +0100

    Add solution day6.

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -4,18 +4,51 @@
     {
         static void Main(string[] args)
         {
-            var input = File.ReadAllLines("input.txt");
+            var lanternFish = File.ReadAllText("input.txt").Split(',').Select(int.Parse);
 
-            PartOne();
-            PartTwo();
+            PartOne(lanternFish.ToList());
+            PartTwo(lanternFish.ToList());
         }
 
-        private static void PartOne()
+        private static void PartOne(List<int> lanternFish)
         {
+            Console.WriteLine(CalculateNumberOfFishAfterXDays(lanternFish, 80));
         }
 
-        private static void PartTwo()
+        private static void PartTwo(List<int> lanternFish)
         {
+            Console.WriteLine(CalculateNumberOfFishAfterXDays(lanternFish, 256));
+        }
+
+        private static long CalculateNumberOfFishAfterXDays(List<int> lanternFish, int days)
+        {
+            var fishGroupedBySpawnTimer = new long[9];
+            foreach (var fish in lanternFish)
+            {
+                fishGroupedBySpawnTimer[fish]++;
+            }
+
+            for (int i = 0; i < days; i++)
+            {
+                long originalZeroes = fishGroupedBySpawnTimer[0];
+
+                for (int j = 0; j <= 7; j++)
+                {
+                    fishGroupedBySpawnTimer[j] = fishGroupedBySpawnTimer[j + 1];
+                }
+
+                fishGroupedBySpawnTimer[6] += originalZeroes;
+                fishGroupedBySpawnTimer[8] = originalZeroes;
+            }
+
+            long numberOfLanternFish = 0;
+            for (int i = 0; i < fishGroupedBySpawnTimer.Length; i++)
+            {
+                numberOfLanternFish += fishGroupedBySpawnTimer[i];
+            }
+
+            return numberOfLanternFish;
+
         }
     }
 }
