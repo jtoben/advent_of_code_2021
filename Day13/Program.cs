7c8e3e46e37a72045d6bd98db8edcac0cb45348c
Joost Toben <joosttoben@hotmail.com>
2021-12-14 08:52:36 +0100

    Add base projects for days 12-16.

---
+++ Day13/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines("input.txt");
+
+            PartOne();
+            PartTwo();
+        }
+
+        private static void PartOne()
+        {
+        }
+
+        private static void PartTwo()
+        {
+        }
+    }
+}
